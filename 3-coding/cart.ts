00784175b412facc320727fe2f34131b9f153533
eao github <bonsai.eao@gmail.com>
2023-11-13 00:12:58 +0700

    chore: add scripts and nodemon

--- 3-coding/cart.ts
+++ 3-coding/cart.ts
@@ -1,4 +1,4 @@
-class Cart {
+export class Cart {
   private items: Record<string, number> = {};
 
   private constructor(private customer_id: string) {}
@@ -40,17 +40,3 @@ class Cart {
     return this.items;
   }
 }
-
-// Example usage:
-const cart = Cart.create("123");
-cart.add("001", 2);
-cart.add("002", 1);
-console.log("Initial Cart:", cart.getItems());
-
-cart.update("001", 5);
-console.log("Updated Cart:", cart.getItems());
-
-cart.remove("002");
-console.log("Cart after removing product 002:", cart.getItems());
-
-cart.destroy();
--- 3-coding/index.ts
+++ 3-coding/index.ts
@@ -1,5 +1,17 @@
 const world = "world";
 
-export function hello(who: string = world): string {
-  return `Hello ${who}! `;
-}
+import { Cart } from "./cart";
+
+// Example usage:
+const cart = Cart.create("123");
+cart.add("001", 2);
+cart.add("002", 1);
+console.log("Initial Cart:", cart.getItems());
+
+cart.update("001", 5);
+console.log("Updated Cart:", cart.getItems());
+
+cart.remove("002");
+console.log("Cart after removing product 002:", cart.getItems());
+
+cart.destroy();
