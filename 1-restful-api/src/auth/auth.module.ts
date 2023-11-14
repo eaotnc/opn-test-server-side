ad571bcec8c26fc3a104d35ecb07e8a82cbae6b3
eao github <bonsai.eao@gmail.com>
2023-11-14 17:41:16 +0700

    fix: remove auth gard

--- 1-restful-api/src/auth/auth.module.ts
+++ 1-restful-api/src/auth/auth.module.ts
@@ -4,12 +4,10 @@ import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { jwtConstants } from './constants';
-import { AuthGuard } from './auth.guard';
 
 @Module({
   imports: [
     UsersModule,
-    AuthGuard,
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
